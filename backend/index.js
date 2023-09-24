f27566dda5f0e042096fe413fd6998c4c5859ebb
Rinzler2603 <hardikrana2003@gmail.com>
2023-09-24 14:37:05 +0530

    connection to db established

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const connectToMongo=require('./db');
+const express = require('express')
+
+console.log("Connected to server successfully");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+connectToMongo();
+
+// Available Routes
+app.use('/api/auth',require('./routes/auth'))
+// app.use('/api/notes',require('./routes/notes'))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Rane')
+})
+
+app.listen(port,()=>{
+    console.log("Listening at port http");
+})
