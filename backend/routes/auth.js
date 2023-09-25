3f0dd0032a658cc5ebe3dd2efa4972c84b6ec8f7
Rinzler2603 <hardikrana2003@gmail.com>
2023-09-26 01:40:13 +0530

    getting user data suing auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const { body,validationResult } = require('express-validator');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+const fetchUser = require("../middleware/fetchUser")
+
 
 const JWT_SECRET = "goodboyhardik";
 
@@ -13,8 +15,8 @@ const valid = [
     body('password').isLength({min:5})
 ]
 
-
-router.post('/',valid, async (req,res)=>{
+// ROUTE 1 : Create a User using /register
+router.post('/register',valid, async (req,res)=>{
 //  If there are errors then return a bad req
     const errors=validationResult(req);
     if(!errors.isEmpty()){
@@ -49,6 +51,62 @@ router.post('/',valid, async (req,res)=>{
         res.json({authToken});
     }
     catch(error){
+        console.log(error);
+        res.status(500).send("Error Occured");
+    }
+})
+
+
+
+
+
+// ROUTE 2 : Creating a login
+router.post('/login',[
+        body("password","Pass can't be blank").exists()
+    ], async (req,res)=>{
+
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+
+//  Creating login verification
+    const {email,password}=req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).send("Wrong email or password ");
+        }
+
+        const pwCompare= bcrypt.compare(password,user.password);
+        if(!pwCompare){
+            return res.status(400).send("Wrong email or password ");
+        }
+
+        const data={
+            user:{
+                id: user.id
+            }
+        }
+
+        const authToken = jwt.sign(data,JWT_SECRET);
+
+        res.json({authToken});
+    } catch(error){
+        console.log(error);
+        res.status(500).send("Error Occured");
+    }
+
+})
+
+// ROUTE 3 : Fetching User Data 
+
+router.post('/getuser',fetchUser,async (req,res)=>{
+    try {
+        userId = req.user.id
+        const user = await User.findById(userId).select("-password")
+        res.send(user)
+    } catch(error){
         console.log(error);
         res.status(500).send("Error Occured");
     }
